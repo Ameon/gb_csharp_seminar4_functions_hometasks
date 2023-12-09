93f08691d6d8ef8fdd1b8a3c04eb2ce892731644
Михаил Гаврилюк <ameon@ya.ru>
2023-12-09 11:33:04 +0500

    Init

---
+++ task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 2:
+//  Задайте массив заполненный случайными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// Установим размер массива
+int size = 5;
+int[] array = new int[size];
+Random random = new Random();
+
+// Заполняем массив случайными трехзначными числами
+for(int i = 0; i < array.Length; i++)
+{
+  array[i] = random.Next(100,1000);
+}
+
+// Выводим массив
+PrintArray(array);
+
+// Подсчитываем количество четных чисел
+int evenCount = 0;
+foreach (int number in array)
+{
+  if (number % 2 == 0)
+  {
+    evenCount++;
+  }
+}
+
+// Выведем результат
+Console.WriteLine($"Количество четных числе в массиве: {evenCount}");
+
+void PrintArray(int[] array)
+{
+  Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+
+
---
+++ task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: 
+//  Напишите программу, которая перевернёт одномерный массив 
+//  (первый элемент станет последним, второй – предпоследним и т.д.)
+
+// Зададим исходный массив
+int[] originalArray = { 1, 2, 3, 4, 5 };
+int[] reversedArray = ReverseArray(originalArray);
+
+Console.WriteLine("Исходный массив: " + String.Join(", ", originalArray));
+Console.WriteLine("Перевернутый массив: " + String.Join(", ", reversedArray));
+
+int[] ReverseArray(int[] array)
+{
+  int[] newArray = new int[array.Length];
+  for (int i = 0; i < array.Length; i++)
+  {
+    newArray[i] = array[array.Length - 1 - i];
+  }
+  return newArray;
+}
